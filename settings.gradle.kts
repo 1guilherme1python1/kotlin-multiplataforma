81f5a7479643aa829a4d0c393efcff452ba9f403
Guilherme Lemos Vieira <guilhermebomdespacholemos@gmail.com>
2024-10-01 12:45:15 -0500

    primeiro commit

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "km_learn_multi"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
